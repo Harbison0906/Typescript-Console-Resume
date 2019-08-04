b126f51221c58befa8de0b9179705f6ccefd0841
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-04 17:34:51 -0500

    added function to call previous positions

--- app.js
+++ app.js
@@ -4,17 +4,40 @@ let resume = {
   Description: 'I love music and movie trivia'
 }
 
-console.log(
+console.log(         //list
   'Name: ' + resume.Name.toUpperCase() + "\n" +
   'Career: ' + resume.Career + "\n" +
   'Description: ' + resume.Description
 );
 
-let myInterests = ['mid-centurty architecture', 'movies', 'running']
-console.log('')
-console.log('My Interests:')
+let myInterests = ['Mid-century architecture', 'Movies', 'Drums']
+console.log('');
+console.log('My Interests:');
 console.log(
-  '- ' + myInterests[0] + "\n" +
-  '- ' + myInterests[1] + "\n" +
-  '- ' + myInterests[2] + "\n"
+  '* ' + myInterests[0] + "\n" +
+  '* ' + myInterests[1] + "\n" +
+  '* ' + myInterests[2] + "\n"
 );
+
+console.log('');
+
+console.log('My Previous Experience:');
+
+function displayPosition(company, title, description) {
+  console.log(
+    '* ' + company + ' | ' + 
+    title + ' | ' +
+    description
+  );
+}
+
+displayPosition('Lewis Thomason', 'Legal Courier', 'Deliver documents to court houses, clients, other firms; issue/serve subpoenas; Notary Public ');
+
+displayPosition('Books-A-Million', 'Magazine Specialist', 'Resposible for keeping magazines up to date and organized');
+
+displayPosition('Printers and Stationers, Inc.', 'Warehouse Employee', 'Assemble, deliver, and install office furniture for small and large businesses');
+
+console.log('');
+
+
+
