96a95bc9bafbd0811a9f9c86776e6c6a22195ecf
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-08-04 16:16:57 -0500

    added info and interests to resume

---
+++ app.js
@@ -0,0 +1,20 @@
+let resume = {
+  Name: 'seth harbison',
+  Career: 'Accounting Assistant',
+  Description: 'I love music and movie trivia'
+}
+
+console.log(
+  'Name: ' + resume.Name.toUpperCase() + "\n" +
+  'Career: ' + resume.Career + "\n" +
+  'Description: ' + resume.Description
+);
+
+let myInterests = ['mid-centurty architecture', 'movies', 'running']
+console.log('')
+console.log('My Interests:')
+console.log(
+  '- ' + myInterests[0] + "\n" +
+  '- ' + myInterests[1] + "\n" +
+  '- ' + myInterests[2] + "\n"
+);
